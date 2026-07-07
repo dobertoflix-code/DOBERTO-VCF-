7c43275c071c125f8c7973a573e6a61f4769333a
DOBERTO MR LIT TECH <dobertoflix@gmail.com>
2026-07-07 10:51:44 -0400

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,3 @@
+// Vercel sèvi ak fichye sa a kòm "serverless function".
+// Li senpleman pran app Express ki nan server.js epi li kite Vercel jere requests yo.
+module.exports = require('../server.js');
